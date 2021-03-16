7d4d916cd4185136555f5a9879593d4059e8f944
NipunDas <43912314+NipunDas@users.noreply.github.com>
2021-03-15 21:19:33 -0700

    started testing trajectories after getting constants

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.controller.RamseteController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
@@ -17,8 +15,6 @@ import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
-
-import edu.wpi.first.wpilibj.util.Units;
 
 import java.util.Arrays;
 
@@ -32,10 +28,6 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
   private static DriveTrain m_drive;
 
   private static Joystick leftJoy, rightJoy;
@@ -72,20 +64,22 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(2), Units.feetToMeters(2));
+    TrajectoryConfig config = new TrajectoryConfig(DriveTrain.maxVelocity, DriveTrain.maxAcceleration);
     Trajectory trajectory = TrajectoryGenerator.generateTrajectory(Arrays.asList(new Pose2d(),
-    new Pose2d(1.0, 1.0, new Rotation2d(-45)), 
+    new Pose2d(0.0, 1.0, new Rotation2d(0))), 
+    /*
     new Pose2d(2.0, 2.0, new Rotation2d(0)), 
     new Pose2d(1.0, 3.0, new Rotation2d(45)),
     new Pose2d(-1.0, 5.0, new Rotation2d(45)), 
     new Pose2d(-2.0, 6.0, new Rotation2d(0)), 
     new Pose2d(-1.0, 7.0, new Rotation2d(-45)),
     new Pose2d(0.0, 8.0, new Rotation2d(0))), 
+    */
     config);
 
     RamseteCommand autoCommand = new RamseteCommand(trajectory, m_drive::getPosition, new RamseteController(2.0, 0.7), 
     m_drive.getFeedforward(), m_drive.getKinematics(), m_drive::getWheelSpeeds, m_drive.getLeftController(), 
-    m_drive.getRightController(), m_drive::tankDrive, m_drive);
+    m_drive.getRightController(), m_drive::setVoltages, m_drive);
     
     // An ExampleCommand will run in autonomous
     return autoCommand;
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -21,7 +21,6 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.wpilibj.util.Units;
 
 public class DriveTrain extends SubsystemBase {
 
@@ -30,16 +29,18 @@ public class DriveTrain extends SubsystemBase {
 
   private AHRS navx = new AHRS(SPI.Port.kMXP);
   Pose2d position;
+  //Ultrasonic ultra = new Ultrasonic();
 
   //constants that need to be adjusted (rough estimates right now)
-  double kWidth = Units.inchesToMeters(28);
-  double kGearRatio = 3;
-  double kWheelRadius = 6;
-  double kTicksInRotation = 1440;
-  double kS = 0.268;
-  double kV = 1.89;
-  double kA = 0.243;
-  double kP = 9.95;
+  double kWidth = 0.607;
+  double kGearRatio = 1;
+  double kWheelRadius = 0.076;
+  double kTicksInRotation = 4096;
+  double kS = 0.983;
+  double kV = 3.34;
+  double kA = 0.517;
+  double kP = 2.14;
+  public static double maxVelocity = 0.5, maxAcceleration = 0.5;
 
   DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(kWidth);
   DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(getHeading());
@@ -60,8 +61,14 @@ public class DriveTrain extends SubsystemBase {
   }
 
   public void tankDrive(double leftPow, double rightPow) {
-    left.set(ControlMode.PercentOutput, leftPow/12);
-    right.set(ControlMode.PercentOutput, rightPow/12);
+    left.set(ControlMode.PercentOutput, leftPow);
+    right.set(ControlMode.PercentOutput, rightPow);
+  }
+
+  public void setVoltages(double leftPow, double rightPow) {
+    left.set(ControlMode.PercentOutput, -1 * leftPow/12);
+    right.set(ControlMode.PercentOutput, -1 * rightPow/12);
+    System.out.println("Voltage: " + leftPow);
   }
 
   public void resetEncoders() {
@@ -70,11 +77,11 @@ public class DriveTrain extends SubsystemBase {
   }
 
   public double getLeftEncoderDistance() {
-    return left.getSelectedSensorPosition(0) * 2 * Math.PI * kWheelRadius / (kTicksInRotation * kGearRatio);
+    return -1 * left.getSelectedSensorPosition(0) * 2 * Math.PI * kWheelRadius / (kTicksInRotation * kGearRatio);
   }
 
   public double getRightEncoderDistance() {
-    return right.getSelectedSensorPosition(0) * 2 * Math.PI * kWheelRadius / (kTicksInRotation * kGearRatio);
+    return -1 * right.getSelectedSensorPosition(0) * 2 * Math.PI * kWheelRadius / (kTicksInRotation * kGearRatio);
   }
 
   public Rotation2d getHeading() {
@@ -87,8 +94,8 @@ public class DriveTrain extends SubsystemBase {
 
   public DifferentialDriveWheelSpeeds getWheelSpeeds() {
     return new DifferentialDriveWheelSpeeds(
-      left.getSensorCollection().getPulseWidthVelocity() * 20 * Math.PI * kWheelRadius / (kGearRatio * kTicksInRotation),
-      right.getSensorCollection().getPulseWidthVelocity() * 20 * Math.PI * kWheelRadius / (kGearRatio * kTicksInRotation)
+      -1 * left.getSensorCollection().getPulseWidthVelocity() * 20 * Math.PI * kWheelRadius / (kGearRatio * kTicksInRotation),
+      -1 * right.getSensorCollection().getPulseWidthVelocity() * 20 * Math.PI * kWheelRadius / (kGearRatio * kTicksInRotation)
     );
   }
 
@@ -113,5 +120,6 @@ public class DriveTrain extends SubsystemBase {
     position = odometry.update(getHeading(), getLeftEncoderDistance(), getRightEncoderDistance());
     // This method will be called once per scheduler run
     tankDrive(RobotContainer.getLeftJoy().getY(), RobotContainer.getRightJoy().getY());
+    //System.out.println(getWheelSpeeds().leftMetersPerSecond);
   }
 }
